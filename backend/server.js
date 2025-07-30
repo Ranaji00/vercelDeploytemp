674815be7fc1198f545b96350a1dd724dcdf1506
Ranaji00 <rananeeru67@gmail.com>
2025-07-30 21:17:03 +0530

    mm

--- backend/server.js
+++ backend/server.js
@@ -11,16 +11,10 @@ import orderRouter from "./routes/orderRoute.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 const port = process.env.PORT || 5000;
 
-// // ✅ Configure CORS
-// const allowedOrigins = [
-//   'https://vercel-admin.vercel.app',
-//   'https://vercel-frontend.vercel.app', // your frontend
-//   "http://localhost:5173"
-//   ,'http://localhost:5175/' // dev frontend
-// ];
+
 
 app.use(cors({
   origin: true,
